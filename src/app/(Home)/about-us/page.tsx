93688bef60b198429fbf40e337d26704eb68da82
rayhan <rayhan.rhssoe2@gmail.com>
2024-03-24 18:48:35 +0700

    fix: remove empty classNames.

--- src/app/(Home)/about-us/page.tsx
+++ src/app/(Home)/about-us/page.tsx
@@ -28,19 +28,19 @@ export default async function About() {
 					<h1 className="text-4xl">From Our CEO</h1>
 				</div>
 				<div className="flex flex-col gap-4  sm:w-4/6">
-					<p className="">
+					<p>
 						Visi LearnSphere adalah menjadi platform edukasi teknologi terdepan yang
 						mendorong akses literasi digital yang lebih luas untuk semua. LearnSphere
 						memiliki misi untuk mengakselerasi transisi Indonesia menuju dunia digital
 						melalui pendidikan teknologi yang mentransformasi kehidupan.
 					</p>
-					<p className="">
+					<p>
 						Kini semua bangsa bergerak menuju dunia digital yang bertumpu pada inovasi
 						teknologi di semua sendi kehidupan. Kami yakin pendidikan teknologi adalah
 						fondasi bagi setiap bangsa agar menjadi yang terdepan dalam menghadapi
 						dunia digital.
 					</p>
-					<p className="">
+					<p>
 						LearnSphere hadir sebagai platform pendidikan teknologi yang membantu
 						menghasilkan talenta digital berstandar global. Semua demi mengakselerasi
 						Indonesia agar menjadi yang terdepan.
